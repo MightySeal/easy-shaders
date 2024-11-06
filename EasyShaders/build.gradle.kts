a8b755ae630be689221dc1722dd2460ba4cbda1a
Konstantin Zolotov <konstantinzolotov@gmail.com>
2024-11-06 22:03:24 +0000

    Configure library module

--- EasyShaders/build.gradle.kts
+++ EasyShaders/build.gradle.kts
@@ -1,31 +1,12 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "io.easyshaders.lib"
-    compileSdk = 34
+    compileSdk = 35
 
-    defaultConfig {
-        applicationId = "io.easyshaders.lib"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "io.easyshaders"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.easyshaders"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
