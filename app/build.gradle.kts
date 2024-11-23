41e47ac096c64192940af3c7a97124f408220cce
Victoria Grinberg <31312528+vicgcode@users.noreply.github.com>
2024-11-23 23:18:06 +0300

    Add material icons library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.accompanist.permissions)
     implementation(libs.accompanist.permissions)
     implementation(libs.androidx.camera.core)
