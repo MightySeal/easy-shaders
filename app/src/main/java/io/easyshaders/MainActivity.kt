06106e75d2e5ff1ec35382920072e4982a8ea17e
Victoria Grinberg <31312528+vicgcode@users.noreply.github.com>
2025-01-24 14:09:00 +0300

    Compose camera app (#7)
    * Add material icons library
    
    * Add CameraPreview
    
    * Add take image methods
    
    * Update LegacyCameraScreen
    
    * Add camera basic funs
    
    * Refactoring
    
    * Update ui
    
    * Merge fixes
    
    * Temp disable startPreview
    
    * Update viewmodel
    
    * Refactoring
    
    * Remove unused imports
    
    * Refactoring

--- app/src/main/java/io/easyshaders/MainActivity.kt
+++ app/src/main/java/io/easyshaders/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.easyshaders
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,16 +8,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
 import io.easyshaders.legacy.LegacyCameraScreen
 import io.easyshaders.ui.theme.EasyShadersTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        if (!hasRequiredPermissions()) {
+            requestPermissions(
+                this, CAMERAX_PERMISSIONS, 0
+            )
+        }
         setContent {
             EasyShadersTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -28,5 +39,22 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun hasRequiredPermissions(): Boolean {
+        return CAMERAX_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CameraScreenPreview() {
+    EasyShadersTheme {
+        LegacyCameraScreen(Modifier)
     }
 }
